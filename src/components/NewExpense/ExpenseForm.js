ee8177726e5eb6bfaad18673b5cbd3c90e035fd0
vic <victoria.bruno14@gmail.com>
2023-08-02 11:16:23 +0200

    feat: :sparkles: Add  change handlers, submit handler and comments for learning

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,20 +1,74 @@
-import { React} from "react";
+import { React, useState} from "react";
 import "./ExpenseForm.css"
 
 export const ExpenseForm = () => {
-  return <form>
+  const [enteredTitle, setEnteredTitle] = useState(" ") 
+  const [enteredAmount, setEnteredAmount] = useState(" ") 
+  const [enteredDate, setEnteredDate] = useState(" ") 
+  
+  // Option to combine all inputs in one object
+  // const [userInput, setUserInput]=useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // })
+
+// Here I am making sure that tha change of state depends on the previous state and not the initial state at load 
+// For each key in the useState object
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value)
+    // Uncomment lines below and comment the line above in case of using the object in the useState
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: event.target.value};
+    // })
+  }
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+    // Uncomment lines below and comment the line above in case of using the object in the useState
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: event.target.value};
+    // })
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value)
+    // Uncomment lines below and comment the line above in case of using the object in the useState
+    // setUserInput((prevState) => {
+    //   return {...prevState, enteredTitle: event.target.value};
+    // })
+  }
+
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseData = {
+    title : enteredTitle,
+    amount : enteredAmount,
+    date : new Date(enteredDate) 
+    };
+    console.log(expenseData);
+
+    // Here I am clearing the form for new inputs
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');  
+  };
+
+  return <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
     <div className="new-expense__control">
     <label>Title</label>
-    <input type="text" />
+    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
     </div>
     <div className="new-expense__control">
     <label>Amount</label>
-    <input type="number" min='0.01' step='0.01'/>
+    <input type="number" min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}/>
     </div>
     <div className="new-expense__control">
     <label>Date</label>
-    <input type="date"  min='2019-01-01' max='2033-12-31'/>
+    <input type="date"  min='2019-01-01' max='2033-12-31' value={enteredDate} onChange={dateChangeHandler}/>
     </div>
     </div>
     <div className='new-expense__actions'>
