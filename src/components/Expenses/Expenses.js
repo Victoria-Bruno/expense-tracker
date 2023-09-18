e914388229cfabce27e81fe05c65e00f6e57171a
vic <victoria.bruno14@gmail.com>
2023-09-18 14:50:01 +0200

    feat: :sparkles: Render items by year (working filter by year)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,18 +10,31 @@ const Expenses = (props) => {
   setFilterYear(selectedYear)
 }
 
+//This is going to return true if the years match, thus only those items will be kept
+const filteredExpenses = props.item.filter(expense => {
+  return expense.date.getFullYear().toString() === filterYear;
+});
+
+
+let expensesOutput = <p>No expenses found</p>
+
+if (filteredExpenses.length > 0) {
+  expensesOutput = filteredExpenses.map((expense) => 
+  <ExpenseItem 
+    key={expense.id}
+    title={expense.title} 
+    amount={expense.amount} 
+    date={expense.date}
+  />
+)
+} 
+
+//Using map I transform the object in App.js with the entries to the JSX element <ExpenseItem>
+//Using && operator conditional rendering if I have no items in filtered year
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
-      
-      {/* Using map I transform the object in App.js with the entries to the JSX element <ExpenseItem>  */}
-      {props.item.map((expense, index) => <ExpenseItem 
-      key={expense.id}
-      title={expense.title} 
-      amount={expense.amount} 
-      date={expense.date}
-      />
-      )}
+      {expensesOutput}
     </Card>
   );
 };
