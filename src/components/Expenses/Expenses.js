7f6a5f0fc540d7fbcfcd2081db8979b60f4fafa7
vic <victoria.bruno14@gmail.com>
2023-09-20 12:09:09 +0200

    refactor: :art: Separate component to render items list
    Separate component to render list from the one that filters items per year. If statement as a consequence can be changed (changing all return completely rather than partially).

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,11 @@
 import {React, useState}  from 'react';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from '../ExpenseFilter/ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
-  const [filterYear, setFilterYear] = useState("2020");
+  const [filterYear, setFilterYear] = useState("");
   const filterChangeHandler = (selectedYear) => { //that we receive as a parameter
   setFilterYear(selectedYear)
 }
@@ -15,26 +15,12 @@ const filteredExpenses = props.item.filter(expense => {
   return expense.date.getFullYear().toString() === filterYear;
 });
 
-
-let expensesOutput = <p>No expenses found</p>
-
-if (filteredExpenses.length > 0) {
-  expensesOutput = filteredExpenses.map((expense) => 
-  <ExpenseItem 
-    key={expense.id}
-    title={expense.title} 
-    amount={expense.amount} 
-    date={expense.date}
-  />
-)
-} 
-
 //Using map I transform the object in App.js with the entries to the JSX element <ExpenseItem>
 //Using && operator conditional rendering if I have no items in filtered year
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
-      {expensesOutput}
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 };
