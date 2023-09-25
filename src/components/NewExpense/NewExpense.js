8f3efc9fb84ca43a0964ef7f21f6a189fbb81f17
vic <victoria.bruno14@gmail.com>
2023-09-25 20:28:34 +0200

    feat: :sparkles: Add initial button at start
    Conditional rendering of the form. Need to complete this feature

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { ExpenseForm } from "./ExpenseForm";
 import "./NewExpense.css"
 
@@ -6,6 +6,21 @@ import "./NewExpense.css"
 //pass to the child the function in the parent from props
 //pass to the parent the enriched parameter (expenseData) to the child
 export const NewExpense = (props) => {
+    const [onDisplay, setOnDisplay] = useState('')
+
+    //Setting a parament that will tell us when the (starting) button is clicked
+    const [clickStatus, setClickStatus] = useState('unclicked')
+     const clickHandler = (event) => {
+        if(clickStatus === 'unclicked') {
+            setClickStatus('clicked')
+        }
+        }
+    
+    //Assigning the JSX to a dynamic variable that is returned
+    let cardOutput = <div className="new-expense">
+    <button onClick={clickHandler}>Add New Expense</button>
+    </div>
+
     const saveExpenseDataHandler = (enteredExpenseData) => {    //this argument will come from the child component (check ExpenseForm)
         const expenseData = {
         ...enteredExpenseData, 
@@ -14,8 +29,16 @@ export const NewExpense = (props) => {
     props.onAddExpense(expenseData)
 };
 
-    return <div className='new-expense'>
-    <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-};
+//I am missing how to reset to initial state - the else if here is wrong
+    if (clickStatus === 'clicked') {
+        return <div className='new-expense'>
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    } else if (cardOutput === null) {
+        setClickStatus('unclicked')
+    }
 
+    return (<div>
+        {cardOutput}
+    </div>)
+}
